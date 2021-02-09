53afdf356151f95fe2b6c1e963b691906186fee9
Tinkergit <nicholas.stagno@gmail.com>
2021-02-09 17:33:05 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,37 @@
-# code here!
+class School
+    attr_accessor :school, :name
+    
+    
+   
+    def roster
+        @roster
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        list={}
+        @roster.each do |key, value|
+            list[key]=value.sort
+            
+        end
+        list
+    end
+
+
+    def initialize(school)
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        if(roster[grade]) ## if @roster has a grade do this
+            @roster[grade] << name
+        ###@roster.merge(student_hash)
+        else
+            @roster[grade] = []
+            @roster[grade] << name
+        end
+    end
+end
